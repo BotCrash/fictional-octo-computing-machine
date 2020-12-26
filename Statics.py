facde2c040866b8b0c38dd509cb842dd3fdbdd67
Frey <74185756+Fr3y@users.noreply.github.com>
2020-12-26 15:20:57 +0530

    issues

---
+++ Statics.py
@@ -0,0 +1,11 @@
+#python as an static
+import numpy as np
+data = [15,16,8,19,22,24,29,30,34]
+print("mean:" np.mean(data))
+print("median:",np.median(dada))
+print("50th percentile(median):",np.percentile(data,50))
+print("25th Percintile:",np.percentile(data,25))
+print("75th Percintile:",np.percentile(data,75))
+print("standard deviation:",np.std(data))
+print("Variance:",np.var(data))
+print("BY REDADDI")
